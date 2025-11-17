59bf0ab9990bb0a26d7b53aaf547638219457a6b
Muddasir Nisar <mnisar@teresol.com>
2025-11-17 16:48:35 +0500

    Defined User, Driver and Trip models relationships. Added Login Controller and api route files.

---
+++ backend/app/Http/Controllers/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function submit(Request $request){
+
+        // Validate the phoen number
+
+        // find or create user model
+
+        // send the user a one-time use code
+
+        // return response
+    }
+}
--- backend/app/Models/Driver.php
+++ backend/app/Models/Driver.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Driver extends Model
 {
-    
+    protected $guarded = [];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function trips(){
+        return $this->hasMany(Trip::class);
+    }
 }
