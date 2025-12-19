daf16bb8defac1f4a3f98b8d015f83da6904ff01
Muddasir Nisar <mnisar@teresol.com>
2025-12-19 16:24:27 +0500

    standby view file

--- frontend/vue-app/src/views/StandByView.vue
+++ frontend/vue-app/src/views/StandByView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="pt-16">
-        <h1 class="text-3xl font-semibold mb-4">Waiting for ride request...</h1>
+        <h1 class="text-3xl font-semibold mb-4">{{ title }}</h1>
         <div class="mt-8 flex justify-center">
             <Loader />
         </div>
@@ -8,6 +8,28 @@
 </template>
 <script setup>
 import Loader from '@/components/Loader.vue';
+import { onMounted, ref } from 'vue';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
+const title = ref("Waiting for ride request...")
 
+onMounted(() => {
+    
+    let echo = new Echo({
+        broadcaster: 'pusher',
+        key: 'mykey',
+        cluster: 'mt1',
+        wsHost: window.location.hostname,
+        wsPort: 6001,
+        forceTLS: false,
+        disableStats: true,
+        enabledTransports: ['ws', 'wss'],
+    });
+
+    echo.channel('drivers')
+        .listen('TripCreated', (e) => {
+            console.log("TripCreated event: ", e);
+        })
+}); 
 </script>
